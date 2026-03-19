b90765058c1776fcac803e9a5c61bb6e2ae1aaed
Matthew Richards <matthewrichards234@gmail.com>
2026-03-19 16:02:38 -0400

    feat: connect to mongodb via mongoose.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
 import { userRouter } from "./routes/users";
 
 const app: Express = express();
 const PORT = 3000;
+
+mongoose
+  .connect(
+    process.env.MONGO_URI || "mongodb://localhost:27017/discount-notifications",
+  )
+  .then(() => console.log("Connected to MongoDB via Mongoose"))
+  .catch((err) => console.error(err));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from Bun + Express!!!");
