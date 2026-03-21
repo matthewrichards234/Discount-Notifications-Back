05bd2a1397abbce0dfbc276b1dadadf61975baa5
Matthew Richards <matthewrichards234@gmail.com>
2026-03-21 18:35:29 -0400

    fix: Add type to ClothingItem schema attributes.

--- src/models/clothingItems.ts
+++ src/models/clothingItems.ts
@@ -1,10 +1,23 @@
 import mongoose from "mongoose";
 
 const clothingItemSchema = new mongoose.Schema({
-  name: {},
-  size: {},
-  brand: {},
-  price: {},
+  name: {
+    type: String,
+    require: true,
+  },
+  size: {
+    type: String,
+    require: true,
+    maxLength: 4,
+  },
+  brand: {
+    type: String,
+    require: true,
+  },
+  price: {
+    type: Number,
+    require: true,
+  },
 });
 
 export const ClothingItem = mongoose.model("ClothingItem", clothingItemSchema);
