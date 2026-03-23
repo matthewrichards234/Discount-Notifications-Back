0b73ecb9abaf1500af412f1816ec477b1afa8d05
Matthew Richards <matthewrichards234@gmail.com>
2026-03-22 20:36:23 -0400

    feat: Add validation middleware and schema for clothing items

--- src/routes/clothingItems.ts
+++ src/routes/clothingItems.ts
@@ -7,11 +7,13 @@ import {
   likeClothingItem,
   dislikeClothingItem,
 } from "../controllers/clothingItems";
+import { validate } from "../middleware/validate";
+import { ClothingItem } from "../schemas/clothingItems";
 export const clothingItemRouter = Router();
 
 clothingItemRouter.get("/TEST", TEST);
 clothingItemRouter.get("/", getAllClothingItems);
 clothingItemRouter.get("/:id", getClothingItemById);
-clothingItemRouter.post("/upload", createClothingItem);
+clothingItemRouter.post("/upload", validate(ClothingItem), createClothingItem);
 clothingItemRouter.patch("/:id/likes", likeClothingItem);
 clothingItemRouter.delete("/:id/likes", dislikeClothingItem);
