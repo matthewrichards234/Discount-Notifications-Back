a85ee9cbb5cc837609412ec020636540dabfb077
Matthew Richards <matthewrichards234@gmail.com>
2026-04-03 17:40:34 -0400

    feat: install cors - Add console logs for testing.

--- src/routes/url.ts
+++ src/routes/url.ts
@@ -3,5 +3,7 @@ import express, { Router } from "express";
 export const urlRouter = Router();
 
 urlRouter.post("/create", (req, res) => {
+  console.log("Received Headers:", req.headers);
+  console.log("Received Body:", req.body);
   res.send("posting new route for playwright to use later");
 });
